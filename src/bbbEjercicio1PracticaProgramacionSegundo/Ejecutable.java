182b8a6aa7ddc486831af6d34b1e47d0a63d2638
seceji@gmail.com <seceji@gmail.com>
2023-02-28 22:19:37 +0100

    casa 28/02/23

--- src/bbbEjercicio1PracticaProgramacionSegundo/Ejecutable.java
+++ src/bbbEjercicio1PracticaProgramacionSegundo/Ejecutable.java
@@ -9,10 +9,10 @@ public class Ejecutable{
 
         Electrodomestico[] listaElectrodomesticos = new Electrodomestico[5];
 
-        listaElectrodomesticos[0] = new Lavadora(12,4,Colores.azul,ConsumoEnergetico.A,123,34);
-        listaElectrodomesticos[1] = new Lavadora();
+        listaElectrodomesticos[0] = new Lavadora(12,4,Colores.blanco,ConsumoEnergetico.A,123,34);
+        listaElectrodomesticos[1] = new Lavadora(12, 4, Colores.blanco, ConsumoEnergetico.C, 345, 34);
         listaElectrodomesticos[2] = new Television(12,23,3,Colores.azul,ConsumoEnergetico.C,34,false);
-        listaElectrodomesticos[3] = new Television();
+        listaElectrodomesticos[3] = new Television(45, 34, 4, Colores.blanco, ConsumoEnergetico.D, 54, true);
         listaElectrodomesticos[4] = new Television();
 
 
