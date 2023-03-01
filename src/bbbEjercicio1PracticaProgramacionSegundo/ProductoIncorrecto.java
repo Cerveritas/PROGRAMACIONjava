6b3cc8beb31a8503c0d0ce253e0d0fb4dee8f996
seceji@gmail.com <seceji@gmail.com>
2023-03-01 19:14:31 +0100

    casa 01/03/23

--- src/bbbEjercicio1PracticaProgramacionSegundo/ProductoIncorrecto.java
+++ src/bbbEjercicio1PracticaProgramacionSegundo/ProductoIncorrecto.java
@@ -1,6 +1,8 @@
 package bbbEjercicio1PracticaProgramacionSegundo;
 
 
+import bbbEjercico2PracticaProgramacionSegundo.ExepcionCuadrado;
+
 public class ProductoIncorrecto extends Exception {
 
     ProductoIncorrecto (String mensaje){
