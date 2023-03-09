fe9d73eeafedb18ad06bf50c4e351f91019a8d79
SergioCerveraJ <seceji@gmail.com>
2023-03-09 13:59:37 +0100

    sesion 09/03/23

--- src/solucionExamenEjercicio1Segundo/Emisora.java
+++ src/solucionExamenEjercicio1Segundo/Emisora.java
@@ -4,12 +4,12 @@ public abstract class Emisora implements Emitible {
 
     // atributos  <-- al hacerlo static lo hacemos atributos clase, que todos los objetos comparten estas varibale
 
-    protected static int numero_emisoras=0;
-    protected static int identificador;
-    protected static String nombre;
-    protected static int anyo_creacion;
-    protected static boolean emitiendo;
-    protected static int num_oyentes;
+    protected int numero_emisoras=0;
+    protected int identificador;
+    protected String nombre;
+    protected int anyo_creacion;
+    protected boolean emitiendo;
+    protected int num_oyentes;
 
 
 
